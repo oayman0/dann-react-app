825205033476fda4236362fec318304844b2035f
Jimmy137 <ahmed_551400@yahoo.com>
2022-06-28 23:34:21 +0200

    home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,7 +8,11 @@ import AppBar from '../components/common/AppBar'
 import FooterSocial from '../components/common/FooterSocial'
 import RightBar from '../components/explore/RightBar'
 import {Redirect} from 'react-router-dom'
-
+import Post from '../components/home/Post'
+import PostInput from '../components/others/PostInput'
+import profile1 from '../assets/images/profile1.png';
+import profile2 from '../assets/images/profile2.jpg';
+import postimage from '../assets/images/product/product1.png';
 
 export class HomePage extends Component {
 
@@ -39,10 +43,11 @@ export class HomePage extends Component {
                          <SidebarDesktop />  </Col>
 
                     <Col lg={6} md={6} sm={12}xs={12} className="min-vh-100">
-{/* Insert Content Here */}
-
-
-{/* End Of Content Here */}      
+                         <div  style={{ margin: 20 }}>
+                              <PostInput />
+                         </div>
+                         <Post displayName="Omar Ayman" username="omar_96" text="New sales in Apprel House store" avatar = {profile1}/> 
+                         <Post displayName="Ahmed Gamal" username="jimmy" text="I just bought this" avatar = {profile2} image={postimage}/> 
                     </Col> 
 
                     <Col lg={4} md={4} sm={4} xs={4}>      
